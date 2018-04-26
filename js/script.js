f239ad6e5c8b8b5117872adb89f9f81363d88817
Libby Schumacher-Knight <libby@fluxfederation.com>
2018-04-27 10:51:27 +1200

    Tidy up how text is showing up on mouseover

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-    // $(".room").hover(function(){
-    //     $(this).css("fill", "yellow");
-    //     }, function(){
-    //     $(this).css("fill", "pink");
-    // });
-
-
-
     $(".room").mouseover(function() {
       console.log($(this));
         $(".room-info").show();
@@ -16,7 +8,5 @@ $(document).ready(function(){
     $(".room").mouseout(function() {
       console.log($(this));
       $(".room-info").empty();
-      // $(".room-info").hide();
     });
-
 });
