5e21ad58582a852aafc9bfc0eb565035a762c6b7
Libby Schumacher-Knight <libby@fluxfederation.com>
2018-04-26 16:25:47 +1200

    an attempt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("test");
+$(document).ready(function(){
+    $(".room").hover(function(){
+        $(this).css("fill", "yellow");
+        }, function(){
+        $(this).css("fill", "pink");
+    });
+
+    $(".room").hover(function() {
+        $( this ).append( $( "<span> ***</span>" ) );
+      }, function() {
+        $( this ).find( "span:last" ).remove();
+    });
+
+});
